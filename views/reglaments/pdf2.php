e780f0f9d0c03855bdfa55627f13689397d7e920
k1osha <k1osha@derpy.ru>
2020-10-04 12:07:18 +1000

    temp changes

--- views/reglaments/pdf2.php
+++ views/reglaments/pdf2.php
@@ -3,14 +3,14 @@
 $pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
 
 // set document information
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 052');
-$pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+$pdf->SetCreator("Зачем");
+$pdf->SetAuthor('Зачем');
+$pdf->SetTitle($model->message);
+$pdf->SetSubject("Зачем");
+$pdf->SetKeywords('Павел Кучин, Андрей Койков, Даша Соколова, Артём Ипатов');
 
 // set default header data
-$pdf->SetHeaderData(PDF_HEADER_LOGO, PDF_HEADER_LOGO_WIDTH, PDF_HEADER_TITLE.' 052', PDF_HEADER_STRING);
+$pdf->SetHeaderData("zachem", 1, "zachem".' zachem', "zachem");
 
 // set header and footer fonts
 $pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
@@ -52,21 +52,21 @@ NOTES:
 // set document signature
 
 // set font
-$pdf->SetFont('helvetica', '', 12);
+$pdf->SetFont('dejavusans', '', 12);
 
 // add a page
 $pdf->AddPage();
 
 // print a line of text
-$text = 'This is a <b color="#FF0000">digitally signed document</b> using the default (example) <b>tcpdf.crt</b> certificate.<br />To validate this signature you have to load the <b color="#006600">tcpdf.fdf</b> on the Arobat Reader to add the certificate to <i>List of Trusted Identities</i>.<br /><br />For more information check the source code of this example and the source code documentation for the <i>setSignature()</i> method.<br /><br /><a href="http://www.tcpdf.org">www.tcpdf.org</a>';
-$pdf->writeHTML($text, true, 0, true, 0);
+$text = 'Это <b color="#FF0000">Образец подписанного ПДФ файла созданного по регламенту</b> c использованием системных ключей подписи ( для примера ) Чтобы проверить, не изменялись ли данные после подписи, вам нужно открыть PDF в Acrobat Reader и посмотреть свойства документа</a>';
+// $try = $model->message;
+$text .=  '<br/>'.'<br/>' . $model->message . '<br/>';
 
+$pdf->writeHTML($text, true, 0, true, 0);
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 // *** set signature appearance ***
 
 // create content for signature (image and/or text)
-$pdf->Image('images/tcpdf_signature.png', 180, 60, 15, 15, 'PNG');
-
 // define active area for signature appearance
 $pdf->setSignatureAppearance(180, 60, 15, 15);
 
