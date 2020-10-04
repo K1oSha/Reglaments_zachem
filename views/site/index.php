9db5caaaa51737b38a9614c78ed947f7c1f09ad3
k1osha <k1osha@derpy.ru>
2020-10-04 13:34:13 +1000

    index upd

--- views/site/index.php
+++ views/site/index.php
@@ -1,3 +1,15 @@
 
 
-<h1>Welcome</h1>
+<h1>
+можно залогиниться за секретаря по данным `rmkcyy@creatnew.pw` : `secretary`  
+</h1>
+
+<h1>
+можно залогиниться за сотрудника прокуратуры по данным `prokuratura@mailper.work` : `prokuratura`  
+</h1>
+
+<h1>
+
+`https://www.1day.email/mailbox/rmkcyy@creatnew.pw` ссылка для проверки почты секретаря
+`https://www.1day.email/mailbox/prokuratura@mailper.work` ссылка для проверки почты прокуратуры
+</h1>
